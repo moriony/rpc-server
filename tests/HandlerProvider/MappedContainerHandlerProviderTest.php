5d098cd3435c60f7ca7bbc492b3dbfcd3ecd1c77
moriony <demateus@gmail.com>
2015-08-14 16:30:47 +0300

    add mapped container handler provider test

---
+++ tests/HandlerProvider/MappedContainerHandlerProviderTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Moriony\RpcServer\Handler;
+
+use Moriony\RpcServer\HandlerProvider\MappedContainerHandlerProvider;
+use Symfony\Component\DependencyInjection\Container;
+
+class MappedContainerHandlerProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testProvideHandlersClass()
+    {
+        $container = new Container();
+        $provider = new MappedContainerHandlerProvider($container, [
+            'map' => [
+                'handlerName' => [
+                    'service' => 'serviceName',
+                    'method' => 'methodName'
+                ]
+            ]
+        ]);
+
+        $handlers = $provider->provide();
+        $this->assertInstanceOf('Moriony\RpcServer\Handler\ServiceContainerHandler', $handlers['handlerName']);
+    }
+
+    /**
+     * @dataProvider provideHanlersCountTestData
+     */
+    public function testProvideHandlersCounts($config, $expectedCount)
+    {
+        $container = new Container();
+        $provider = new MappedContainerHandlerProvider($container, $config);
+        $this->assertCount($expectedCount, $provider->provide());
+    }
+
+    public function provideHanlersCountTestData()
+    {
+        return [
+            // Case 1
+            [
+                [
+                    'map' => []
+                ],
+                0
+            ],
+            // Case 2
+            [
+                [
+                    'map' => [
+                        'handlerName' => [
+                            'service' => 'serviceName',
+                            'method' => 'methodName'
+                        ]
+                    ]
+                ],
+                1
+            ],
+
+            // Case 3
+            [
+                [
+                    'map' => [
+                        'handlerName1' => [
+                            'service' => 'serviceName',
+                            'method' => 'methodName'
+                        ],
+                        'handlerName2' => [
+                            'service' => 'serviceName',
+                            'method' => 'methodName'
+                        ]
+                    ]
+                ],
+                2
+            ],
+        ];
+    }
+}
