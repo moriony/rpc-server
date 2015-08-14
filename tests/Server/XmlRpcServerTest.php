a360a515fc54179f3cf13610902f6b7888441c53
moriony <demateus@gmail.com>
2015-08-14 17:54:14 +0300

    add xml rpc server tests

--- tests/Server/XmlRpcServerTest.php
+++ tests/Server/XmlRpcServerTest.php
@@ -3,19 +3,19 @@
 namespace Moriony\RpcServer\ResponseSerializer;
 
 use Moriony\RpcServer\Exception\InvalidMethodException;
-use Moriony\RpcServer\Request\JsonRpcRequest;
-use Moriony\RpcServer\Response\JsonRpcResponse;
-use Moriony\RpcServer\Server\JsonRpcServer;
+use Moriony\RpcServer\Request\XmlRpcRequest;
+use Moriony\RpcServer\Response\XmlRpcResponse;
+use Moriony\RpcServer\Server\XmlRpcServer;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 
-class JsonRpcServerTest extends \PHPUnit_Framework_TestCase
+class XmlRpcServerTest extends \PHPUnit_Framework_TestCase
 {
     protected $server;
 
     public function setUp()
     {
-        $this->server = new JsonRpcServer($this->createEventDispatcherMock(), $this->createSerializerMock());
+        $this->server = new XmlRpcServer($this->createEventDispatcherMock(), $this->createSerializerMock());
     }
 
     public function testCreateRequest()
@@ -24,7 +24,7 @@ class JsonRpcServerTest extends \PHPUnit_Framework_TestCase
         $request = $this->createHttpRequest();
         $rpcRequest = $method->invoke($this->server, $request);
 
-        $this->assertInstanceOf('Moriony\RpcServer\Request\JsonRpcRequest', $rpcRequest);
+        $this->assertInstanceOf('Moriony\RpcServer\Request\XmlRpcRequest', $rpcRequest);
         $this->assertSame($request, $rpcRequest->getHttpRequest());
     }
 
@@ -32,28 +32,21 @@ class JsonRpcServerTest extends \PHPUnit_Framework_TestCase
     {
         $method = $this->setMethodAccessible($this->server, 'createResponse');
 
-        /** @var JsonRpcResponse $rpcResponse */
+        /** @var XmlRpcResponse $rpcResponse */
         $rpcResponse = $method->invoke($this->server, 'test_content');
 
-        $this->assertInstanceOf('Moriony\RpcServer\Response\JsonRpcResponse', $rpcResponse);
+        $this->assertInstanceOf('Moriony\RpcServer\Response\XmlRpcResponse', $rpcResponse);
         $this->assertSame(200, $rpcResponse->getStatusCode());
-        $this->assertSame('application/json', $rpcResponse->headers->get('Content-Type'));
+        $this->assertSame('application/xml', $rpcResponse->headers->get('Content-Type'));
         $this->assertSame('test_content', $rpcResponse->getContent());
     }
 
     public function testPrepareResponseData()
     {
         $method = $this->setMethodAccessible($this->server, 'prepareResponseData');
-        $request = $this->createRpcRequestMock(['getId']);
-        $request->method('getId')->willReturn('test_id');
-
-        $responseData = $method->invoke($this->server, $request, 'test_result');
-        $expected = [
-            'jsonrpc' => '2.0',
-            'result' => 'test_result',
-            'id' => 'test_id'
-        ];
-        $this->assertEquals($expected, $responseData);
+        $request = $this->createRpcRequestMock();
+        $responseData = $method->invoke($this->server, $request, 'test_data');
+        $this->assertEquals('test_data', $responseData);
     }
 
     /**
@@ -83,11 +76,11 @@ class JsonRpcServerTest extends \PHPUnit_Framework_TestCase
             ->getMock();
     }
     /**
-     * @return \PHPUnit_Framework_MockObject_MockObject|JsonRpcRequest
+     * @return \PHPUnit_Framework_MockObject_MockObject|XmlRpcRequest
      */
     public function createRpcRequestMock(array $methods = [])
     {
-        return $this->getMockBuilder('Moriony\RpcServer\Request\JsonRpcRequest')
+        return $this->getMockBuilder('Moriony\RpcServer\Request\XmlRpcRequest')
             ->disableOriginalConstructor()
             ->setMethods($methods)
             ->getMock();
@@ -95,15 +88,10 @@ class JsonRpcServerTest extends \PHPUnit_Framework_TestCase
 
     public function createHttpRequest()
     {
-        $content = json_encode([
-            'jsonrpc' => "2.0",
-            'method' => 'methodName',
-            'params' => [],
-            'id' => 'test',
-        ]);
+        $content = xmlrpc_encode_request('methodName', []);
         $request = new Request([], [], [], [], [], [], $content);
         $request->setMethod('POST');
-        $request->headers->set('Content-Type', 'application/json');
+        $request->headers->set('Content-Type', 'application/xml');
         return $request;
     }
 
@@ -122,26 +110,16 @@ class JsonRpcServerTest extends \PHPUnit_Framework_TestCase
             [
                 new \Exception('test message', 123),
                 [
-                    'jsonrpc' => '2.0',
-                    'error' => [
-                        'code' => -32603,
-                        'message' => 'Unexpected error occurred.',
-                        'data' => null
-                    ],
-                    'id' => null
+                    'faultCode' => -32603,
+                    'faultString' => 'Unexpected error occurred.',
                 ]
             ],
             // Case 2
             [
                 new InvalidMethodException('test message'),
                 [
-                    'jsonrpc' => '2.0',
-                    'error' => [
-                        'code' => -32601,
-                        'message' => 'test message',
-                        'data' => null
-                    ],
-                    'id' => null
+                    'faultCode' => -32601,
+                    'faultString' => 'test message',
                 ]
             ],
         ];
