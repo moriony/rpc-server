63ead6c490861210be4023b02837697c833ad361
moriony <demateus@gmail.com>
2015-09-22 19:34:22 +0300

    add exclusion to ExceptionLoggerSubscriber

--- src/Subscriber/ExceptionLoggerSubscriber.php
+++ src/Subscriber/ExceptionLoggerSubscriber.php
@@ -10,10 +10,16 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class ExceptionLoggerSubscriber implements EventSubscriberInterface
 {
     protected $logger;
+    protected $exclusion;
 
-    public function __construct(LoggerInterface $logger)
+    /**
+     * @param LoggerInterface $logger
+     * @param mixed $exclusion
+     */
+    public function __construct(LoggerInterface $logger, $exclusion = null)
     {
         $this->logger = $logger;
+        $this->exclusion = $exclusion;
     }
 
     public static function getSubscribedEvents()
@@ -25,6 +31,16 @@ class ExceptionLoggerSubscriber implements EventSubscriberInterface
 
     public function onException(ExceptionEvent $event)
     {
+        $exclusion = $this->exclusion;
+        if (!is_null($exclusion)) {
+            if (!is_callable($exclusion)) {
+                throw new \RuntimeException('Invalid exclusion.');
+            }
+            if (call_user_func($exclusion, $event)) {
+                return;
+            }
+        }
+
         $this->logger->error($event->getException()->getMessage(), [
             'exception' => $event->getException()
         ]);
--- tests/Subscriber/ExceptionLoggerSubscriberTest.php
+++ tests/Subscriber/ExceptionLoggerSubscriberTest.php
@@ -32,6 +32,62 @@ class ExceptionLoggerSubscriberTest extends \PHPUnit_Framework_TestCase
         $subscriber->onException(new ExceptionEvent($request, $exception));
     }
 
+    public function testExclusionException()
+    {
+        $this->setExpectedException('RuntimeException', 'Invalid exclusion.');
+
+        $exception = new \Exception('test');
+
+        $logger = $this->createLoggerMock();
+        $subscriber = new ExceptionLoggerSubscriber($logger, 'invalid_function');
+
+        $request = $this->createRequestMock();
+        $subscriber->onException(new ExceptionEvent($request, $exception));
+    }
+
+    public function testExclusionCall()
+    {
+        $phpunit = $this;
+        $exception = new \Exception('test');
+        $request = $this->createRequestMock();
+        $event = new ExceptionEvent($request, $exception);
+        $subscriber = new ExceptionLoggerSubscriber($this->createLoggerMock(), function ($e) use($phpunit, $event) {
+            $phpunit->assertSame($e, $event);
+        });
+        $subscriber->onException($event);
+    }
+
+    public function testExclusionFail()
+    {
+        $exception = new \Exception('test');
+
+        $logger = $this->createLoggerMock();
+        $logger->expects($this->once())
+            ->method('error');
+
+        $subscriber = new ExceptionLoggerSubscriber($logger, function () {
+            return false;
+        });
+        $request = $this->createRequestMock();
+        $subscriber->onException(new ExceptionEvent($request, $exception));
+    }
+
+    public function testExclusionSuccess()
+    {
+        $exception = new \Exception('test');
+
+        $logger = $this->createLoggerMock();
+        $logger->expects($this->exactly(0))
+            ->method('error');
+
+        $subscriber = new ExceptionLoggerSubscriber($logger, function () {
+            return true;
+        });
+
+        $request = $this->createRequestMock();
+        $subscriber->onException(new ExceptionEvent($request, $exception));
+    }
+
     /**
      * @return \PHPUnit_Framework_MockObject_MockObject|LoggerInterface
      */
